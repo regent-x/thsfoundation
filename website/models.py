32861f4bdd672f8e029f53ab1d6420baddbb96da
regent-x <spiritofdamola@gmail.com>
2024-01-15 17:36:42 +0100

    models fixed

--- website/models.py
+++ website/models.py
@@ -1,4 +1,37 @@
 from django.db import models
 
-# Create your models here.
+from phonenumber_field.modelfields import PhoneNumberField
 
+# Create your models here.
+class Investor(models.Model):
+    CHOICE = (
+            ('MALE', 'male'),
+            ('FEMALE', 'female'),
+            )
+
+    RELIGION = (
+            ('CHRISTIAN', 'christian'),
+            ('MUSLIM', 'muslim'),
+            ('OTHERS', 'others'),
+            )
+
+    Firstname = models.CharField(max_length=15)
+    Surname = models.CharField(max_length=15)
+    Othername = models.CharField(max_length=15)
+    Gender = models.CharField(max_length=15, choices=CHOICE, default="male")
+    Religion = models.CharField(max_length=15, choices=RELIGION, default="christian")
+
+    Phone_Number = PhoneNumberField()
+    Whatsapp_Number = PhoneNumberField()
+
+    class Seeds(models.IntegerChoices):
+        One = 1
+        Two = 2
+        Three = 3
+        Four = 4
+        Five = 5
+
+    Seed = models.IntegerField(choices=Seeds)
+    Address = models.TextField()
+    Hobbies = models.TextField()
+    Ref_id = models.IntegerField(default=0000)
