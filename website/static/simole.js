f31df2862a7f9d6501ece0af412e3dda571e9fd8
regent-x <spiritofdamola@gmail.com>
2024-01-17 02:42:54 +0100

    Uploading to server

---
+++ website/static/simole.js
@@ -0,0 +1,12 @@
+function generate4DigitNumber() {
+    // Generate the random number
+    const randomNumber = Math.floor(Math.random() * 9000) + 1000;
+
+    // Ensure 4-digit string with leading zeros
+
+    form = document.getElementById("myForm")
+    form.addEventListener("submit",(event) => {
+        event.PreventDefault()
+        form:
+
+}
