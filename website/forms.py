cb6f7e377f118eb094b3fecde6f77f93493bec40
Adewole Damole <spiritofdamola@gmai.com>
2024-01-16 17:49:36 -0800

    unknown

--- website/forms.py
+++ website/forms.py
@@ -1,17 +1,8 @@
 from django import forms
-<<<<<<< HEAD
+
 from .models import Investor
 
 class InvestorForm(forms.ModelForm):
     class Meta:
         model = Investor
         fields = '__all__'
-=======
-
-from .models import Investor
-
-class InvestorForm(forms.Form):
-    class Meta:
-        model = Investor
-        fields = '__all__'
->>>>>>> refs/remotes/origin/main
